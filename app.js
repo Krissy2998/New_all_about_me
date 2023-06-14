8fbab28747edc2b41042e60cdc9e86a3108bf550
christol <christol.beresford@hotmail.co.uk>
2023-06-14 15:02:51 +0100

    Updated to include guessing game

---
+++ app.js
@@ -0,0 +1,24 @@
+// All questions are Yes or No. or Y/N. Use .toUpperCase
+
+let userName = prompt("What is your name?");
+let userNameReply = alert("Hi " + userName);
+let userFeelings = prompt("How are you feeling today?");
+let userFeelingsReply = alert("Yeah, I feel " + userFeelings + " sometimes too");
+let userAge = prompt("How old are you?");
+let userAgeReply= alert ("Okay, that's cool!");
+let userChoice = prompt("Would you like to know a bit about me?");
+let userChoiceReply= alert("Awesome, lets get started ,read on to find out more!");
+
+//Yes-no questions.
+function guessingGame(
+    ){
+let myHeight= prompt("Am I 5'4?");
+if (myHeight==="yes") {
+    alert ("Well done " + userName);
+}
+ myHeightReply= alert ("Oh, not quite!");
+}
+
+//missing - add alerts to give response to user.
+//add alert "response to user input" check chat. 
+ 
