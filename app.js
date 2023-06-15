3dbe5fe40a06cf0e3b88d622fcebcc9a570564f1
christol <christol.beresford@hotmail.co.uk>
2023-06-15 16:07:50 +0100

    added y/n questions + number guessing game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // All questions are Yes or No. or Y/N. Use .toUpperCase
-
+console.log("guessing game")
+''
 let userName = prompt("What is your name?");
 let userNameReply = alert("Hi " + userName);
 let userFeelings = prompt("How are you feeling today?");
@@ -12,13 +13,63 @@ let userChoiceReply= alert("Awesome, lets get started ,read on to find out more!
 //Yes-no questions.
 function guessingGame(
     ){
-let myHeight= prompt("Am I 5'4?");
-if (myHeight==="yes") {
-    alert ("Well done " + userName);
+function myHeight(
+){
+        let myHeight= prompt("Am I 5'4?").toLowerCase(); 
+        while (myHeight!="yes" && myHeight!="no"){
+            alert ("Please answer yes or no");
+            myHeight= prompt("Am I 5'4?") .toLowerCase();
+        }
+        if (myHeight==="yes") {
+            alert ("Well done " + userName);
+        } else if (myHeight==="no"){
+        alert ("That's right, I am 5'4")
+    }else {
+        alert ("please answer yes or no");
+    }}
+myHeight();
+function kidsNames(
+){
+    let mySonName= prompt ("Is my son called Malvin?");
+if (mySonName==="No") {
+alert ("Correct! His name is Ez!");    
+}else alert("That's right, his name is Ez")
+let myDaughterName= prompt("Is my Daughter called Malvina?");
+if (myDaughterName==="No"){
+    alert("Correct! her name is Ky!");   
+} else alert("That's not right, her name is Ky");
 }
- myHeightReply= alert ("Oh, not quite!");
-}
+kidsNames();
+function myHobbies(
+){
+let myHobby=prompt("Do I rollerskate?");
+if (myHobby==="yes"){
+    alert("That's right, it's alot of fun!");
+}   else alert("Not quite right, try again");
+};
+myHobbies();
 
+let correctNumber = 9;
+let attempt = 4
+
+
+while (attempt > 0) {
+let guess = +prompt("guess number between 1-10. You have 4 attempts");
+if (guess === correctNumber) {
+alert("Yay! That's the correct number");
+break;
+} else if (guess > correctNumber){
+alert("Too high, try again");
+} else{
+alert ("Too low! Try again");
+}
+attempt--;
+    }
+    if(attempt===0) {
+        alert("Out of tires, the correct number is 9");
+    }
+
+
+}
 //missing - add alerts to give response to user.
-//add alert "response to user input" check chat. 
- 
+//add alert "response to user input" check chat.
